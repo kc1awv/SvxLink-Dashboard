5bd35be321c6092441813ac39492ed3d89534c70
kc1awv <smiller@kc1awv.net>
2020-04-21 14:58:33 -0400

    initial commit

---
+++ config/config.php
@@ -0,0 +1,19 @@
+<?php
+date_default_timezone_set('America/New_York');
+define("CONTACTEMAIL", "your@email.com");
+define("SVXLOGPATH", "/var/log");
+define("SVXLOGPREFIX", "svxlink");
+define("SVXCONFPATH", "/etc/svxlink");
+define("SVXCONFFILENAME", "svxlink.conf");
+define("SVXLINKPATH", "/usr/bin");
+define("SVXLOGICSECTION", "SimplexLogic");
+define("SVXMODULES", array('EchoLink', 'Parrot', 'TclVoiceMail'));
+define("TIMEZONE", "America/New_York");
+define("REFRESHAFTER", "60");
+define("SHOWPROGRESSBARS", "on");
+define("SHOWOLDMHEARD", "60");
+define("TEMPERATUREALERT", "on");
+define("TEMPERATUREHIGHLEVEL", "85");
+define("SHOWQRZ", "on");
+?>
+
